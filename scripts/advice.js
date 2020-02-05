1ff24b524500ae8e1e8c36d03e3aea1544ba98d5
ellejaiebaie <laura.brennan@mac.com>
2020-02-05 14:58:59 -0800

    as done as its gonna be, styled and working

--- scripts/advice.js
+++ scripts/advice.js
@@ -12,7 +12,7 @@ let slip = {};
 
 function addQuote(object) {
   const quote = object;
-  console.log(quote);
+  // console.log(quote);
 
   let result = document.querySelector(".result__output");
   result.classList.add("unhide");
@@ -20,6 +20,7 @@ function addQuote(object) {
   let quoteText = document.createElement("h2");
   quoteText.innerText = quote;
   quoteText.classList.add("result__text");
+  quoteText.setAttribute("id", "output");
 
   result.appendChild(quoteText);
 }
@@ -28,9 +29,13 @@ const button = document.querySelector(".button");
 
 button.addEventListener("click", showQuote => {
   showQuote.preventDefault();
+  var div = document.querySelector(".result__output");
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
   let result = axios.get("https://api.adviceslip.com/advice").then(response => {
     let slip = response.data.slip.advice;
-    console.log(response.data.slip.advice);
+    // console.log(response.data.slip.advice);
     addQuote(slip);
   });
 });
